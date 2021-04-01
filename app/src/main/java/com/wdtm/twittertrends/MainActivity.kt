fa40bdaca7571a86dec374d78897a59ec43cd82b
florekkinga <florek.kinga@gmail.com>
2021-04-01 18:16:31 +0200

    add simple layout with working google map

--- app/src/main/java/com/wdtm/twittertrends/MainActivity.kt
+++ app/src/main/java/com/wdtm/twittertrends/MainActivity.kt
@@ -2,10 +2,31 @@ package com.wdtm.twittertrends
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.google.android.gms.maps.CameraUpdateFactory
+import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.OnMapReadyCallback
+import com.google.android.gms.maps.SupportMapFragment
+import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.MarkerOptions
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), OnMapReadyCallback {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val mapFragment = supportFragmentManager
+                .findFragmentById(R.id.map) as SupportMapFragment
+        mapFragment.getMapAsync(this)
+    }
+
+    override fun onMapReady(googleMap: GoogleMap) {
+        val sydney = LatLng(-34.0, 151.0)
+        googleMap.addMarker(
+                MarkerOptions()
+                        .position(sydney)
+                        .title("Marker in Sydney")
+        )
+        googleMap.moveCamera(CameraUpdateFactory.newLatLng(sydney))
+        googleMap.mapType = GoogleMap.MAP_TYPE_HYBRID
     }
 }
