982b6f8e37f304235b595027bec9ed1bf32de95c
florekkinga <florek.kinga@gmail.com>
2021-04-13 18:30:04 +0200

    add request for location permissions and gps

--- app/src/main/java/com/wdtm/twittertrends/MainActivity.kt
+++ app/src/main/java/com/wdtm/twittertrends/MainActivity.kt
@@ -1,7 +1,18 @@
 package com.wdtm.twittertrends
 
-import androidx.appcompat.app.AppCompatActivity
+import android.Manifest
+import android.content.Intent
+import android.content.pm.PackageManager
+import android.location.LocationManager
 import android.os.Bundle
+import android.provider.Settings
+import android.widget.Toast
+import androidx.appcompat.app.AlertDialog
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
+import com.google.android.gms.common.ConnectionResult
+import com.google.android.gms.common.GoogleApiAvailability
 import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.OnMapReadyCallback
@@ -10,6 +21,12 @@ import com.google.android.gms.maps.model.LatLng
 import com.google.android.gms.maps.model.MarkerOptions
 
 class MainActivity : AppCompatActivity(), OnMapReadyCallback {
+
+    private var isLocationPermissionGranted = false
+    private val PERMISSIONS_REQUEST_ACCESS_FINE_LOCATION = 9003
+    private val ERROR_DIALOG_REQUEST = 9001
+    private val PERMISSIONS_REQUEST_ENABLE_GPS = 9002
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -28,5 +45,100 @@ class MainActivity : AppCompatActivity(), OnMapReadyCallback {
         )
         googleMap.moveCamera(CameraUpdateFactory.newLatLng(sydney))
         googleMap.mapType = GoogleMap.MAP_TYPE_HYBRID
+    }
+
+    override fun onResume() {
+        super.onResume()
+        if (checkMapServices()) {
+            if (!isLocationPermissionGranted) {
+                getLocationPermission()
+            }
+        }
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (requestCode == PERMISSIONS_REQUEST_ENABLE_GPS) {
+            if (!isLocationPermissionGranted) {
+                getLocationPermission()
+            }
+        }
+    }
+
+    override fun onRequestPermissionsResult(requestCode: Int,
+                                            permissions: Array<String?>,
+                                            grantResults: IntArray) {
+        isLocationPermissionGranted = false
+        if (requestCode == PERMISSIONS_REQUEST_ACCESS_FINE_LOCATION) {
+            // If request is cancelled, the result arrays are empty.
+            if (grantResults.isNotEmpty()
+                    && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                isLocationPermissionGranted = true
+            }
+        }
+    }
+
+    private fun checkMapServices(): Boolean {
+        if (isServicesOK()) {
+            if (isMapsEnabled()) {
+                return true
+            }
+        }
+        return false
+    }
+
+    private fun isMapsEnabled(): Boolean {
+        val manager = getSystemService(LOCATION_SERVICE) as LocationManager
+
+        if (!manager.isProviderEnabled(LocationManager.GPS_PROVIDER)) {
+            buildAlertMessageNoGps()
+            return false
+        }
+        return true
+    }
+
+    private fun buildAlertMessageNoGps() {
+        val builder = AlertDialog.Builder(this)
+        builder.setMessage("This application requires GPS to work properly, do you want to enable it?")
+                .setCancelable(false)
+                .setPositiveButton("Yes") { dialog, id ->
+                    val enableGpsIntent = Intent(Settings.ACTION_LOCATION_SOURCE_SETTINGS)
+                    startActivityForResult(enableGpsIntent, PERMISSIONS_REQUEST_ENABLE_GPS)
+                }
+        val alert = builder.create()
+        alert.show()
+    }
+
+    private fun isServicesOK(): Boolean {
+
+        val available = GoogleApiAvailability.getInstance().isGooglePlayServicesAvailable(this@MainActivity)
+
+        when {
+            available == ConnectionResult.SUCCESS -> {
+                return true
+            }
+            GoogleApiAvailability.getInstance().isUserResolvableError(available) -> {
+                val dialog = GoogleApiAvailability.getInstance().getErrorDialog(this@MainActivity, available, ERROR_DIALOG_REQUEST)
+                dialog.show()
+            }
+            else -> {
+                Toast.makeText(this, "You can't make map requests", Toast.LENGTH_SHORT).show()
+            }
+        }
+        return false
+    }
+
+    private fun getLocationPermission() {
+        if (ContextCompat.checkSelfPermission(
+                        this.applicationContext,
+                        Manifest.permission.ACCESS_FINE_LOCATION
+                ) == PackageManager.PERMISSION_GRANTED) {
+            isLocationPermissionGranted = true
+        } else {
+            ActivityCompat.requestPermissions(
+                    this,
+                    arrayOf(Manifest.permission.ACCESS_FINE_LOCATION),
+                    PERMISSIONS_REQUEST_ACCESS_FINE_LOCATION)
+        }
     }
 }
